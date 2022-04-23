80bcb417bc45283b571bc73382088275a57785cd
TikhomirovaElvira <elviratikhomirova@mail.ru>
2022-04-23 21:36:28 +0300

    hm class

---
+++ src/ru/skypro/Book.java
@@ -0,0 +1,28 @@
+package ru.skypro;
+
+public class Book {
+    private String nameBook;
+    private String author;
+    private int year;
+
+    public Book(String nameBook, String author, int year) {
+        this.nameBook = nameBook;
+        this.author = author;
+        this.year = year;
+    }
+    public String getNameBook() {
+        return this.nameBook;
+    }
+    public String getAuthor() {
+        return this.author;
+    }
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+
+}
--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,7 +2,29 @@ package ru.skypro;
 
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+
+        Book book = new Book("Nineteen Eighty-Four", "George Orwell", 1948);
+        System.out.println("book.nameBook = " + book.getNameBook());
+        System.out.println("book.author = " + book.getNameBook());
+        System.out.println("book.year = " + book.getYear());
+        book.setYear(1950);
+        System.out.println("book.getYear() = " + book.getYear());
+
+        Book anotherBook = new Book("Brave New World", "Aldous Huxley", 1932);
+        System.out.println("anotherBook = " + anotherBook.getNameBook());
+        System.out.println("anotherBook.author = " + anotherBook.getAuthor());
+        System.out.println("anotherBook.year = " + anotherBook.getYear());
+
+        Author author = new Author("George", "Orwell");
+        System.out.println("author.firstNameAuthor = " + author.getFirstNameAuthor());
+        System.out.println("author.lastNameAuthor = " + author.getLastNameAuthor());
+
+        Author author1 = new Author("Aldous", "Huxley");
+        System.out.println("author1.getFirstNameAuthor = " + author1.getFirstNameAuthor());
+        System.out.println("author1.getLastNameAuthor = " + author1.getLastNameAuthor());
+
+
     }
 }
